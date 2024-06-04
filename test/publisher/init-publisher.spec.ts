b482a93e0627fbe9f89435b2109fefb3a50927dd
Matthew Treiber <0xlarry8@gmail.com>
2024-06-04 17:06:12 -0400

    inital set up for testing
    todo: write tests

---
+++ test/publisher/init-publisher.spec.ts
@@ -0,0 +1,14 @@
+
+describe("INIT PUBLISHER", () => {
+  beforeAll(async () => {
+    
+  });
+
+  it("should create a publisher account", async () => {
+
+  });
+
+  it("should not create a publisher account if already exists", async () => {
+
+  });
+});
---
+++ test/publisher/withdraw-rewards.spec.ts
@@ -0,0 +1,16 @@
+
+describe("WITHDRAQ PUBLISHER REWARDS", () => {
+    beforeAll(async () => {
+        // create publisher account
+        // user accepts offer and converts for SOL
+        // user accepts offer and converts for SPL
+    });
+  
+    it("should withdraw SOL rewards", async () => {
+  
+    });
+  
+    it("should withdraw SPL rewards", async () => {
+  
+    });
+  });
