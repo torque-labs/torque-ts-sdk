41a3c16239c5429f6432b22250783ae8bd36124f
nardevvv <vedran.sisic+personal@gmail.com>
2024-06-07 19:43:23 -0400

    Fix build

---
+++ docs-website/docs/sdk/typedoc-sidebar.cjs
@@ -0,0 +1,323 @@
+// @ts-check
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const typedocSidebar = { items: [
+  {
+    "type": "category",
+    "label": "Enumerations",
+    "items": [
+      {
+        "type": "doc",
+        "id": "sdk/enumerations/ActionType",
+        "label": "ActionType"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/enumerations/ApiEventType",
+        "label": "ApiEventType"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/enumerations/ApiRewardType",
+        "label": "ApiRewardType"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/enumerations/ApiStatus",
+        "label": "ApiStatus"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/enumerations/ApiTxnTypes",
+        "label": "ApiTxnTypes"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/enumerations/Operation",
+        "label": "Operation"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/enumerations/TargetType",
+        "label": "TargetType"
+      }
+    ]
+  },
+  {
+    "type": "category",
+    "label": "Classes",
+    "items": [
+      {
+        "type": "doc",
+        "id": "sdk/classes/TorqueAdminClient",
+        "label": "TorqueAdminClient"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/classes/TorqueAudienceClient",
+        "label": "TorqueAudienceClient"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/classes/TorqueRequestClient",
+        "label": "TorqueRequestClient"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/classes/TorqueSDK",
+        "label": "TorqueSDK"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/classes/TorqueUserClient",
+        "label": "TorqueUserClient"
+      }
+    ]
+  },
+  {
+    "type": "category",
+    "label": "Type Aliases",
+    "items": [
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ActionRequirementsInput",
+        "label": "ActionRequirementsInput"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ActionTarget",
+        "label": "ActionTarget"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ApiAudience",
+        "label": "ApiAudience"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ApiCampaign",
+        "label": "ApiCampaign"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ApiCampaignLeaderboard",
+        "label": "ApiCampaignLeaderboard"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ApiIdentifyPayload",
+        "label": "ApiIdentifyPayload"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ApiInputLogin",
+        "label": "ApiInputLogin"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ApiLinks",
+        "label": "ApiLinks"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ApiRaffleRewards",
+        "label": "ApiRaffleRewards"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ApiResponse",
+        "label": "ApiResponse"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ApiResponseError",
+        "label": "ApiResponseError"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ApiResponseSuccess",
+        "label": "ApiResponseSuccess"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ApiShare",
+        "label": "ApiShare"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ApiUser",
+        "label": "ApiUser"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ApiUserJourney",
+        "label": "ApiUserJourney"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ApiVerifiedUser",
+        "label": "ApiVerifiedUser"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/Audience",
+        "label": "Audience"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/AudienceBuild",
+        "label": "AudienceBuild"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/AudienceBuildResponse",
+        "label": "AudienceBuildResponse"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/AudienceFunctionResponse",
+        "label": "AudienceFunctionResponse"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/BridgeAction",
+        "label": "BridgeAction"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/BridgeActionRequirements",
+        "label": "BridgeActionRequirements"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/CampaignCreateInput",
+        "label": "CampaignCreateInput"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/CampaignEndInput",
+        "label": "CampaignEndInput"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/NftMintAction",
+        "label": "NftMintAction"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/NftMintActionRequirements",
+        "label": "NftMintActionRequirements"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/OpenPositionRequirementsInput",
+        "label": "OpenPositionRequirementsInput"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/OpenPositionTarget",
+        "label": "OpenPositionTarget"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ProgramInteractionAction",
+        "label": "ProgramInteractionAction"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/ProgramInteractionActionRequirements",
+        "label": "ProgramInteractionActionRequirements"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/PublisherCreateInput",
+        "label": "PublisherCreateInput"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/PublisherPayoutInput",
+        "label": "PublisherPayoutInput"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/StakedSolRequirementsInput",
+        "label": "StakedSolRequirementsInput"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/StakedSolTarget",
+        "label": "StakedSolTarget"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/SwapAction",
+        "label": "SwapAction"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/SwapActionRequirements",
+        "label": "SwapActionRequirements"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/Target",
+        "label": "Target"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/TokenHoldingRequirementsInput",
+        "label": "TokenHoldingRequirementsInput"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/TokenHoldingTarget",
+        "label": "TokenHoldingTarget"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/TorqueAdminClientOptions",
+        "label": "TorqueAdminClientOptions"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/TorqueAudienceClientOptions",
+        "label": "TorqueAudienceClientOptions"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/TorqueSDKOptions",
+        "label": "TorqueSDKOptions"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/TorqueUserClientOptions",
+        "label": "TorqueUserClientOptions"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/TxnExecute",
+        "label": "TxnExecute"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/TxnExecuteResponse",
+        "label": "TxnExecuteResponse"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/TxnInput",
+        "label": "TxnInput"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/VoteAction",
+        "label": "VoteAction"
+      },
+      {
+        "type": "doc",
+        "id": "sdk/type-aliases/VoteActionRequirements",
+        "label": "VoteActionRequirements"
+      }
+    ]
+  }
+]};
+module.exports = typedocSidebar.items;
