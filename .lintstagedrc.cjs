a2512c2fa3f57d067481534fe4a270ff69d7812f
nardevvv <vedran.sisic+personal@gmail.com>
2024-06-10 12:07:39 -0400

    change filename

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   // Run type-check on changes to TypeScript files
-  'src/**/*.(ts|tsx)': () => 'npm run typecheck',
+  'src/**/*.ts?(x)': () => 'npm run typecheck',
   // Lint & Prettify TS and JS files
   'src/**/*.(ts|tsx|js)': (filenames) => [
     `npm run lint . ${filenames.join(' ')}`,
