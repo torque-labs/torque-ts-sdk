c761f3a8ea146c9ec00944174060e799ea8055b9
Matthew Treiber <0xlarry8@gmail.com>
2024-06-10 07:49:32 -0400

    wip

--- test/publisher/init-publisher.spec.ts
+++ test/publisher/init-publisher.spec.ts
@@ -1,14 +1,23 @@
+import { Keypair } from "@solana/web3.js";
+import { airdrop, connection } from "../helper";
+import { TorqueClient } from "@/client";
 
 describe("INIT PUBLISHER", () => {
+  const newUser = Keypair.generate();
+  const torque = new TorqueClient({
+    signer: newUser
+  });
   beforeAll(async () => {
-    
+    await airdrop(connection, newUser.publicKey);
   });
 
   it("should create a publisher account", async () => {
-
+    const result = await torque.initPublisher(newUser);
+    expect(result).toBeDefined();
   });
 
   it("should not create a publisher account if already exists", async () => {
-
+    const result = await torque.initPublisher(newUser);
+    expect(result).toBeUndefined();
   });
 });
