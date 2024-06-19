900bdf69d85bb23a69de958e257d9685f4d44c37
nardevvv <vedran.sisic+personal@gmail.com>
2024-06-19 16:19:13 -0400

    Add offers api endpoints

--- src/constants/torque.ts
+++ src/constants/torque.ts
@@ -25,7 +25,7 @@ const TORQUE_API_ROUTES = {
   },
   userCampaigns: '/users/campaigns',
   users: '/users',
-  usersOffers: '/users/offers',
+  usersOffers: '/offers',
 };
 
 const TORQUE_FUNCTIONS_ROUTES = {
